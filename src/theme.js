55e5ee99792619a13eb618b64e85b0f52c50fa8e
MikeLeaderDev <mike@mikextvn88>
2025-10-14 18:48:16 +0700

    update  theme

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,47 @@
 import { createTheme } from '@mui/material/styles';
+import { experimental_extendTheme as extendTheme} from '@mui/material/styles';
 import { red,blue,green } from '@mui/material/colors';
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-    //  main: '#b62d23ff'
-        main: green[700]
-    },
-    secondary: {
-      main: '#1c9185ff'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: red[500]
+// const theme = createTheme({
+//   palette: {
+//     mode: 'dark',
+//     primary: {
+//     //  main: '#b62d23ff'
+//         main: green[700]
+//     },
+//     secondary: {
+//       main: '#1c9185ff'
+//     },
+//     error: {
+//       main: red.A400
+//     },
+//     text: {
+//       secondary: red[500]
+//     }
+//   }
+// })
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: '#ff5252'
+       }
+     }
+   },
+   dark: {
+     palette: {
+       primary: {
+         main: '#1c9185ff'
+       }
+      }
     }
-  }
-})
+  },
+    text: {
+        secondary: red[500]
+    }
+});
 
 export default theme
